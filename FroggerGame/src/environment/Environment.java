2eb5d763f970c92a1c3928f6836d1e53b5776d6c
MahrezAli <93276262+MahrezAli@users.noreply.github.com>
2021-12-03 19:57:30 +0100

    Add files via upload

---
+++ FroggerGame/src/environment/Environment.java
@@ -0,0 +1,144 @@
+package environment;
+
+
+import gameCommons.Game;
+import gameCommons.IEnvironment;
+import gameCommons.Music;
+import util.Case;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Environment implements IEnvironment {
+    private ArrayList<Lane> lane;
+    private Game game;
+    Music soundCoin = new Music();
+    public Environment(Game game){
+        this.game = game;
+        this.lane = new ArrayList<Lane>();
+
+        this.lane.add(new Lane(this.game, 0, 0,true)); //cree la premiere ligne sans voiture
+        for(int i = 1; i < this.game.height -8 ; ++i) {
+            if(i % 5 == 0){
+                this.lane.add(new Lane(this.game, i, 0,true));
+            }else {
+                this.lane.add(new Lane(this.game, i,true));// crée les lignes intermediaires avec des voiture
+            }
+        }
+        for(int i = this.game.height -8; i < this.game.height -1 ; ++i) {
+            if(i % 5 == 0){
+                this.lane.add(new Lane(this.game, i, 0,false));
+            }else {
+                this.lane.add(new Lane(this.game, i, 0.28-game.defaultDensity, false));// crée les lignes intermediaires avec des voiture
+            }
+        }
+        this.lane.add(new Lane(this.game, this.game.height-1 , 0, false)); //creer la derniere ligne sans voiture
+
+        ArrayList<Integer> baba = new ArrayList<>(this.lanePouvantContenirToile(6));
+        for(int i = 0; i < this.lane.size(); i++) {
+            for (int j = 0; j < baba.size(); j++) {
+                if (baba.get(j) == i) {
+                    this.lane.get(i).addToile();
+                }
+            }
+        }
+        ArrayList<Integer> mama = new ArrayList<>(this.lanePouvantContenirCoins(6));
+        for(int i = 0; i < this.lane.size(); i++) {
+            for (int j = 0; j < mama.size(); j++) {
+                if (mama.get(j) == i) {
+                    this.lane.get(i).addCoin();
+                }
+            }
+        }
+    }
+
+    public Lane getLane(int x) {
+        return this.lane.get(x);
+    }
+
+    public boolean isSafe(Case c) {
+        return (this.lane.get(c.ord)).isSafe(c);
+    }
+    public boolean isSafef(Case c) { // safe pour la grenouille
+        return (this.lane.get(c.ord)).isSafeForFrog(c);
+    }
+
+    public void setGame(Game game){
+        this.game = game;
+    }
+    public boolean isWinningPosition(Case c){
+        if(c.ord == this.game.height - 1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public int genererInt(int borneInf, int borneSup){
+        Random random = new Random();
+        int nb;
+        nb = borneInf + random.nextInt(borneSup - borneInf);
+        return nb;
+
+    }
+
+    public ArrayList<Lane> getLane() {
+        return lane;
+    }
+
+    public ArrayList<Integer> lanePouvantContenirToile(int x){
+        int i = 0;
+        ArrayList<Integer>hav = new ArrayList<>();
+        while(i < x){
+            int s = genererInt(1,this.game.height-8);
+            if(this.lane.get(s).getDensity() > 0.0) {
+                hav.add(s);
+                i++;
+            }
+        }
+        return hav;
+    }
+
+    public ArrayList<Integer> lanePouvantContenirCoins(int x){
+        int i = 0;
+        ArrayList<Integer>hav = new ArrayList<>();
+        while(i < x){
+            int s = genererInt(1,this.game.height-8);
+            if(!hav.contains(s) && s % 5 != 0){
+                hav.add(s);
+                i++;
+            }
+        }
+        return hav;
+    }
+
+    public boolean frogSurLaToile(){
+        for(int i = 0; i < this.lane.size(); i++){
+            if(this.lane.get(i).positionToile()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean frogSurLeCoin(){
+        for(int i = 0; i < this.lane.size(); i++){
+            if(this.lane.get(i).positionCoin()){
+                URL test = getClass().getResource("/Music/CoinFrog.wav");
+                this.soundCoin.playSound(test);
+                this.game.score++;
+                this.game.scoreMax++;
+                this.lane.get(i).removeCoin();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void update(){
+        for(int i = 0; i < this.lane.size(); i++){
+            this.lane.get(i).update();
+        }
+    }
+}
---
+++ FroggerGame/src/gameCommonsInf3/FrogInf3.java
@@ -0,0 +1,138 @@
+package gameCommonsInf3;
+
+import gameCommons.Music;
+import gameCommons3.Game3;
+import gameCommons3.IFrog3;
+import util.Case;
+import util.Direction;
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.net.URL;
+
+public class FrogInf3 implements IFrog3 {
+    private Game3 game;
+    private Direction direction;
+    private Case cases;
+    private boolean estEnVie;
+    private boolean restart;
+    int droite = 0;
+    int gauche = 0;
+    int haut = 0;
+    int bas = 0;
+    private Music music = new Music();
+    private int leftToRight;
+
+
+    public FrogInf3(Game3 game){
+        this.game = game;
+        this.direction  = Direction.up;
+        this.cases = new Case(this.game.width / 2,0);
+        this.estEnVie = true;
+    }
+
+    public void setRestart(boolean b){
+        restart = b;
+    }
+
+    public boolean getRestart(){
+        return restart;
+    }
+
+    public void setLife(Boolean b){
+        this.estEnVie = b;
+    }
+    public void setLeftToRight(int leftToRight) {
+        this.leftToRight = leftToRight;
+    }
+
+    public void deplacer(boolean x){
+        if(x) {
+            if(this.leftToRight == 1){
+                this.cases = new Case(cases.absc + 1, cases.ord);
+            }if(this.leftToRight == 0){
+                this.cases = new Case(cases.absc - 1, cases.ord);
+            }
+        }
+    }
+
+    public boolean estPasDansLaGrille() {
+        if(this.cases.absc < 0 || this.cases.absc >= this.game.width){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public void setCases(Case cases) {
+        this.cases = cases;
+    }
+
+    public Case getPosition() {
+        return this.cases;
+    }
+
+    public Direction getDirection() {
+        return this.direction;
+    }
+
+    public boolean estDansLaCase(Case c){
+        if(c.ord != this.cases.ord ){
+            return false;
+        }
+        if(c.absc < this.cases.absc){ // si la case de 'c' est avant celle du radeau
+            return false;
+        }
+        else{
+            if(c.absc >= this.cases.absc){ //si la case 'c' est devant le radeau
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void move(Direction key){
+        URL test = getClass().getResource("/Music/JumpFrog.wav");
+        this.direction = key;
+
+        if (key == Direction.up && this.cases.ord + 1 < this.game.height && estEnVie) {
+            this.cases = new Case(this.cases.absc, this.cases.ord + 1);
+            music.playSound(test);
+            this.game.score++;
+            if (this.game.score >= 3) {
+                if (this.game.score > this.game.scoreMax) {
+                    this.game.scoreMax = this.game.score;
+                    this.game.ajouterLaDerniereLigne();
+                    this.game.height++;
+                }
+            } else {
+                this.game.scoreMax = this.game.score;
+            }
+
+        }
+        if (key == Direction.down && this.cases.ord - 1 >= 0 && estEnVie) {
+            this.cases = new Case(this.cases.absc, this.cases.ord - 1);
+            music.playSound(test);
+            this.game.score--;
+            this.game.height--;
+            bas = 0;
+
+        }
+        if (key == Direction.left && this.cases.absc - 1 >= 0 && estEnVie) {
+            this.cases = new Case(this.cases.absc - 1, this.cases.ord);
+            music.playSound(test);
+            gauche = 0;
+
+        }
+        if (key == Direction.right && this.cases.absc + 1 < this.game.width && estEnVie) {
+            this.cases = new Case(this.cases.absc + 1, this.cases.ord);
+            music.playSound(test);
+            droite = 0;
+
+        }
+        this.game.testLose();
+    }
+}
+
